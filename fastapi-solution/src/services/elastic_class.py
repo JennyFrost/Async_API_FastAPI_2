7af1f2e5e002b66d5428655fe5f5e0ca82354235
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-06-19 17:09:08 +0300

    test commit

--- fastapi-solution/src/services/elastic_class.py
+++ fastapi-solution/src/services/elastic_class.py
@@ -6,9 +6,59 @@ class ElasticMain:
     def __init__(self, elastic: AsyncElasticsearch):
         self.elastic = elastic
 
-    async def get_obj_from_elastic(self, id: str, index: str, some_class) -> BaseModel | None:
+    async def get_obj_from_elastic(self, obj_id: str, index: str, some_class) -> BaseModel | None:
         try:
-            doc = await self.elastic.get(index=index, id=id)
+            doc = await self.elastic.get(index=index, id=obj_id)
         except NotFoundError:
             return None
         return some_class(**doc['_source'])
+
+    async def get_objects_from_elastic(self, page: int, page_size: int, index: str, some_class, sort_field: str = None) -> list[BaseModel]:
+        try:
+            search_body = {"_source": [
+                    "id",
+                    "title",
+                    "imdb_rating"],
+                }
+            search_body.update({"query": {"match_all": {}}})
+            search_body.update(Paginator(page_size=page_size, page_number=page).get_paginate_body())
+            if sort_field:
+                search_body.update(Sort(sort_field=sort_field).get_sort_body())
+            objects = await self.elastic.search(index=index, body=search_body)
+        except NotFoundError:
+            return None
+        return [some_class(**obj['_source']) for obj in objects['hits']['hits']]
+
+
+class Paginator(BaseModel):
+    page_number: int
+    page_size: int
+
+    def get_paginate_body(self) -> dict:
+        start_number = (self.page_number - 1) * self.page_size
+        start_number = start_number if start_number > 0 else 0
+        paginator_body = {
+            "from": start_number,
+            "size": self.page_size
+        }
+        return paginator_body
+
+
+class Sort(BaseModel):
+    sort_field: str
+    order: str = "asc"
+
+    def get_sort_body(self) -> dict:
+        if self.sort_field[0] == '-':
+            self.order = "desc"
+            self.sort_field = self.sort_field[1:]
+        sort_body = {
+            "sort": [
+                    {
+                        self.sort_field: {
+                            "order": self.order
+                        }
+                    }
+                    ]
+        }
+        return sort_body
--- fastapi-solution/src/services/genre.py
+++ fastapi-solution/src/services/genre.py
@@ -19,7 +19,7 @@ class GenreService(CacheMixin):
     async def get_by_id(self, genre_id: str) -> Genre | None:
         genre = await self._object_from_cache(genre_id)
         if not genre:
-            genre = await self.elastic_main.get_obj_from_elastic(genre_id, "genres", Genre)
+            genre = await self.elastic_main.get_obj_from_elastic(obj_id=genre_id, index="genres", some_class=Genre)
             if not genre:
                 return None
             await self._put_object_to_cache(genre, genre_id)
@@ -29,27 +29,13 @@ class GenreService(CacheMixin):
     async def get_genres_list(self, page: int, page_size: int) -> list[Genre]:
         genres = await self._objects_from_cache(f'all_genres_page_{page}_size_{page_size}')
         if not genres:
-            genres = await self._get_genres_from_elastic(page, page_size)
+            genres = await self.elastic_main.get_objects_from_elastic(
+                page=page, page_size=page_size,
+                index='genres', some_class=Genre)
             if not genres:
                 return []
             await self._put_objects_to_cache(genres, f'all_genres_page_{page}_size_{page_size}')
         return genres
-
-    async def _get_genre_from_elastic(self, genre_id: str) -> Genre | None:
-        try:
-            doc = await self.elastic.get(index='genres', id=genre_id)
-        except NotFoundError:
-            return None
-        return Genre(**doc['_source'])
-
-    async def _get_genres_from_elastic(self, page: int, page_size: int) -> list[Genre]:
-        try:
-            search_body = {"query": {"match_all": {}}}
-            search_body.update(Paginator(page_size=page_size, page_number=page).get_paginate_body())
-            genres = await self.elastic.search(index='genres', body=search_body)
-        except NotFoundError:
-            return None
-        return [Genre(**genre['_source']) for genre in genres['hits']['hits']]
     
     async def _objects_from_cache(self, some_id: str) -> list[Genre]:
         objects = await super()._objects_from_cache(some_id)
