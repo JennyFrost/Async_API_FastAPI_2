fc4f11104597c004ccbfe2f21cf37bce237b1723
Кирилл Петухов <test@mail.ru>
2023-06-20 20:55:51 +0300

    fix etl_person

--- fastapi-solution/etl_persons/backoff.py
+++ fastapi-solution/etl_persons/backoff.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 import psycopg2
-import elastic_transport
+from elasticsearch.exceptions import ConnectionError
 
 from pg_work import PostgresLoad
 from es_work import ESWriter
@@ -23,14 +23,14 @@ def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
                             break
                     except psycopg2.OperationalError:
                         sleep(t)
-            except elastic_transport.ConnectionError:
+            except ConnectionError:
                 while True:
                     t = t * 2 if t < border_sleep_time else border_sleep_time
                     try:
                         es = ESWriter(settings.es)
                         es.get_info()
                         break
-                    except elastic_transport.ConnectionError:
+                    except ConnectionError:
                         sleep(t)
         return wrapper
     return inner_decorator
