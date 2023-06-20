cfabe4a4325692171dcf76607b666ff8cc9f2cce
vladimir <vladimir1012kov@gmail.com>
2023-06-20 22:07:07 +0300

    Продолжение работы над структурой тестов

---
+++ fastapi-solution/tests/functional/utils/wait_for_es.py
@@ -0,0 +1,16 @@
+
+from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+import time
+import os
+
+load_dotenv()
+
+host=os.getenv('ELASTIC_HOST')
+port=os.getenv('ELASTIC_PORT')
+
+while True:
+    es_client = Elasticsearch(hosts=f"http://{host}:{port}", validate_cert=False, use_ssl=False)
+    if es_client.ping():
+        break
+    time.sleep(1)
