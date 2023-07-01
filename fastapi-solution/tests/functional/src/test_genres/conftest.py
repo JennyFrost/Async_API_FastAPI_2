e9aa3b868858f28ae485ba9e63f92d89fc763170
JennyFost <jenny1515@mail.ru>
2023-07-02 02:56:31 +0300

    добавила тесты для жанров и фикстуры для них

---
+++ fastapi-solution/tests/functional/src/test_genres/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+
+from ...testdata.film import Film, Genre, PersonBase
+from ...settings import test_settings_genre, test_settings_movies
+
+genres = ["Western",
+          "Adventure",
+          "Drama",
+          "Romance",
+          "Sport",
+          "Talk-Show",
+          "Action",
+          "Thriller",
+          "Comedy",
+          "Family",
+          "Music",
+          "Crime",
+          "Animation",
+          "Sci-Fi",
+          "Documentary",
+          "Musical",
+          "Short",
+          "Fantasy",
+          "War",
+          "Biography",
+          "Mystery",
+          "Reality-TV",
+          "History",
+          "News",
+          "Horror",
+          "Game-Show"]
+
+
+@pytest.fixture
+def generate_genres():
+    async def inner(num_documents,
+                    genre_id: str = None,
+                    name: str = None):
+        if num_documents > len(genres):
+            num_documents = len(genres)
+        documents_genres = []
+        for i in range(num_documents):
+            doc = Genre(
+                id=str(i)*3 if genre_id is None else genre_id,
+                name=genres[i] if name is None else name
+            )
+            documents_genres.append({'index': {'_index': test_settings_genre.es_index,
+                                               '_id': doc.id}})
+            documents_genres.append(doc.dict())
+        return documents_genres
+    return inner
