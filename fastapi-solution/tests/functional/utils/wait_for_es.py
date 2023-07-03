abe35fbdfe4da8bd9a6eb737573600c17dc2f055
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-03 15:46:11 +0300

    wait_for_es

--- fastapi-solution/tests/functional/utils/wait_for_es.py
+++ fastapi-solution/tests/functional/utils/wait_for_es.py
@@ -10,7 +10,7 @@ host=os.getenv('ELASTIC_HOST')
 port=os.getenv('ELASTIC_PORT')
 
 while True:
-    es_client = Elasticsearch(hosts=f"http://{host}:{port}", validate_cert=False, use_ssl=False)
+    es_client = Elasticsearch(hosts=f"http://{host}:{port}")
     if es_client.ping():
         break
     time.sleep(1)
